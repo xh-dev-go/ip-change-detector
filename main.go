c7d680f1416767229bc7a3474faa4bf9196af546
xeth <adamhung12@gmail.com>
2022-04-08 18:22:51 +0800

    add log

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/xh-dev-go/xhUtils/flagUtils/flagString"
 	notification "gitlab.xh-network.xyz/xeth/ip-changed-detector/notificatioin"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
@@ -43,6 +44,8 @@ func writeFile(bytes []byte) {
 }
 
 func main() {
+	log.Println("V1.0.1")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	tokenFlag := flagString.New("token", "token for tg bot").BindCmd()
 	targetId := flagString.New("to", "target id").BindCmd()
 	flag.Parse()
@@ -50,14 +53,19 @@ func main() {
 	fileExist()
 
 	if respons, err := http.Get("https://api.myip.com"); err != nil {
+		log.Println(err.Error())
 		panic(FailRequest)
 	} else if bytes, err := io.ReadAll(respons.Body); err != nil {
+		log.Println(err.Error())
 		panic(FailReadResponse)
 	} else {
-		md5Previouse := fmt.Sprintf("%x", md5.Sum([]byte(readFile())))
+		last := readFile()
+		md5Previouse := fmt.Sprintf("%x", md5.Sum([]byte(last)))
 		md5Now := fmt.Sprintf("%x", md5.Sum(bytes))
 
 		if md5Previouse != md5Now {
+			log.Println("Previous: " + last)
+			log.Println("Now: " + string(bytes))
 			writeFile(bytes)
 			notification.Send(targetId.Value(), string(bytes), tokenFlag.Value())
 		}
